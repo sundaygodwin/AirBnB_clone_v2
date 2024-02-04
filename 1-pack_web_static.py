b3ef9523da0daf10ee67ba9ea3dee78f5eec7e69
sundaygodwin <godwinsunday194@gmail.com>
2024-02-04 12:42:07 -0800

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,20 +5,26 @@ execute: fab -f 1-pack_web_static.py do_pack
 """
 
 from datetime import datetime
-from fabric.api import *
+from fabric.api import local, env
+from fabric.context_managers import lcd
+
+
+env.hosts = ['34.227.101.172', '54.237.100.121']
 
 
 def do_pack():
     """
-    making an archive on web_static folder
+    Making an archive of the web_static folder
     """
 
     time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.tgz'
     local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
+
+    with lcd('versions'):
+        result = local('tar -cvzf {} ../web_static'.format(archive))
+
+    if result.succeeded:
+        return 'versions/{}'.format(archive)
     else:
         return None
-
