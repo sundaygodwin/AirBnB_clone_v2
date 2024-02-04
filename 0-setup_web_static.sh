1ec20319b0c2355c7a34617e131dc07a8e09ae75
sundaygodwin <godwinsunday194@gmail.com>
2024-02-04 14:55:44 -0800

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Set up my web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
@@ -21,29 +22,6 @@ sudo echo "<html>
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-# Update Nginx configuration
-config_content="
-server {
-    listen 80;
-    server_name _;
-
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-    }
-
-    location / {
-        add_header X-Served-By \$HOSTNAME;
-        proxy_pass http://127.0.0.1:5000;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        root /usr/share/nginx/html;
-        internal;
-    }
-}
-"
-
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
