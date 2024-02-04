e9e2aba27889a88ef7611041cad96bbc5b9bec73
sundaygodwin <godwinsunday194@gmail.com>
2024-02-04 15:33:38 -0800

    update

--- depoly.py
+++ depoly.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 """
-Fabric script that creates and distributes an archive to web servers
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes  archive to my web servers
+execute command: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/school -u ubuntu
 """
 
-from fabric.api import env, run
+from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-from fabric.operations import put
-
 env.hosts = ['34.227.101.172', '54.237.100.121']
 
+
 def do_pack():
-    """Generates a tgz archive"""
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
-            local("mkdir -p versions")
+            local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as e:
-        print(f"Error during packaging: {e}")
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
@@ -40,16 +40,15 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        print("New version deployed!")
         return True
-    except Exception as e:
-        print(f"Error during deployment: {e}")
+    except:
         return False
 
 
 def deploy():
-    """Creates and distributes an archive to web servers"""
+    """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
     return do_deploy(archive_path)
+
